d4b716a35ce1467a87aabc51b9d18b7df632b2ff
Juneid Teklu <te.juneid@gmail.com>
2024-10-23 10:31:38 -0500

    Create app express instance

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import "./styles.css";
-/*  */
+const express = require("express");
+const userRouter = require("./controllers/userRouter");
+const app = express();
+
+app.use("/", userRouter);
+
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Express server is active at port ${PORT}!`)
+);
