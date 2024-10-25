bbe191219e1a10d97c17e20e2c5b7125b138c919
Juneid Teklu <te.juneid@gmail.com>
2024-10-25 11:17:22 -0500

    Finish query function that searches db for entered string

--- src/db/queries.js
+++ src/db/queries.js
@@ -8,8 +8,16 @@ async function getAllUsernames() {
 async function insertUsername(username) {
   await pool.query("INSERT INTO usernames (username) VALUES($1)", [username]);
 }
+async function findUser(username) {
+  let { rows } = await pool.query(
+    "SELECT username FROM usernames WHERE username ILIKE $1",
+    ["%" + username + "%"]
+  );
+  return rows;
+}
 
 module.exports = {
   getAllUsernames,
   insertUsername,
+  findUser,
 };
