bda52e8823a182f4511c31d5315d3e5e0c408929
Juneid Teklu <te.juneid@gmail.com>
2024-10-25 10:51:11 -0500

    Create a homePage view and controller that handles the searching of a username

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,9 +1,13 @@
 const expressValidator = require("express-validator");
 const dbQueries = require("../db/queries");
 
-exports.usernameListGet = async (req, res) => {
-  const usernames = await dbQueries.getAllUsernames();
-  res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
+exports.usernameListGet = async (req, res, next) => {
+  if (req.query.search) {
+    next();
+  } else {
+    const usernames = await dbQueries.getAllUsernames();
+    res.render("homePage", { usernames: usernames });
+  }
 };
 
 exports.usernameFormGet = async (req, res) => {
@@ -14,4 +18,10 @@ exports.createUsernamePost = async (req, res) => {
   let username = req.body.username;
   await dbQueries.insertUsername(username);
   res.redirect("/");
+};
+
+exports.usernameSearch = async (req, res) => {
+  let username = req.query.search;
+  let searchResults = await dbQueries.findUser(username);
+  res.render("homePage", { usernames: searchResults });
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,7 +2,10 @@ const { Router } = require("express");
 const userControllers = require("../controllers/userControllers.js");
 const userRouter = Router();
 
-userRouter.get("/", userControllers.usernameListGet);
+userRouter.get("/", [
+  userControllers.usernameListGet,
+  userControllers.usernameSearch,
+]);
 userRouter.get("/new", userControllers.usernameFormGet);
 userRouter.post("/new", userControllers.createUsernamePost);
 
